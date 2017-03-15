6d048b697bc5f11482a00242495afa164842b1ca
hadley <1477546184@qq.com>
2017-03-15 18:22:34 +0800

    gulp task config

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+var NODE_ENV = process.env.NODE_ENV || '';
+
+var isDev = function(){
+  return NODE_ENV.trim() === 'development';
+}();
+
+var isProd = function(){
+  return NODE_ENV.trim() === 'production';
+}();
+
+
+gulp.task('clean', function(cb){
+  var stream = del(['dist/**/*', 'demo/**/*'], cb);
+  return stream;
+});
+
+gulp.task('build', ['clean'], function(){
+  gulp.src('src/template.js')
+      .pipe(gulp.dest('dist/'));
+  gulp.src('src/template.js')
+      .pipe(uglify())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('dist/'));
+});
+
+var taskList = [];
+
+gulp.task('default', taskList);
