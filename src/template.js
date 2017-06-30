7ab106a04d0285c605db06de75b4c5dde7abed5f
hadley <1477546184@qq.com>
2017-06-30 10:39:49 +0800

    update [dev]v2.0.0 template.js

--- src/template.js
+++ src/template.js
@@ -1,69 +1,55 @@
-;(function(win){
+/*!
+ * template - v2.0.0
+ * https://github.com/hexson/template
+ * hexson 2017
+ */
+
+;(function(global){
   'use strict';
 
-  var template = function(source, options){
-    var ele = document.getElementById(source);
-    var tpl = ele && ele.innerHTML || source;
-    if (options.constructor !== Object) throw new Error('The second argument is not a Object');
-    return render(tpl, options);
+  /*!
+   * @name    template
+   * @param   {String}  模板ID
+   * @param   {Object}  模板数据
+   * @return  {String}  渲染好的HTML字符串
+   */
+  var template = function(id, data){
+    if (_cache.hasOwnProperty(id)) return _cache[id](data);
+    var str = document.getElementById(id).innerHTML;
+    _cache[id] = template.render(str);
+    return _cache[id](data);
   };
 
-  template.version = '1.0.1';
 
-  var trim = template.trim = function(str){
-    return str && str.toString ? str.toString().replace(/^\s+/, '').replace(/\s+$/, '') : str;
+  template.version = '2.0.0';
+  template.startTag = '{{';
+  template.endTag = '}}';
+
+
+  var isArray = Array.isArray || function(obj){
+    return ({}).toString.call(obj) === '[object Array]';
   };
 
-  var jsonStringify = template.jsonStringify = function(v){
-    var stringify = '';
-    var curval;
-    if (v === 'null') return String(v);
-    switch(typeof v){
-      case 'number':
-      case 'boolean':
-        return String(v);
-      case 'string':
-        return '"' + v + '"';
-      case 'undefined':
-      case 'function':
-        return undefined;
-    }
-    if (v.constructor === Array){
-      stringify += '[';
-      for (var i = 0; i < v.length - 1; i++){
-        curval = jsonStringify(v[i]);
-        stringify += (curval === undefined ? null : curval) + ',';
-      }
-      stringify += jsonStringify(v[i]) + ']';
-      return stringify;
-    }else if (v.constructor === Object){
-      stringify += '{';
-      for (var i in v){
-        if (v.hasOwnProperty(i)){
-          curval = jsonStringify(v[i]);
-          if (curval !== undefined){
-            stringify += '"' + i + '":' + curval + ',';
-          }
-        }
-      }
-      stringify = stringify.slice(0, -1) + '}';
-      return stringify;
-    }else if (v.constructor === Date){
-      return '"' + (v.toJSON ? v.toJSON() : v.toString()) + '"';
-    }else if (v.constructor === RegExp){
-      return '{}';
-    }
+
+  /*!
+   * @name    trim
+   * @param   {String}   字符串
+   * @return  {String}   删除头尾空格的字符串
+   */
+  var trim = template.trim = function(string){
+    return String(string).replace(/^\s+/, '').replace(/\s+$/, '');
   };
 
-  var tplSetting = template.tplSetting = {
-    openIdentify: '{{',
-    closeIdentify: '}}'
-  };
 
+  /*!
+   * @name    each
+   * @param   {Object}    数组或者对象
+   * @param   {Function}  遍历变量的回调函数
+   */
   var each = template.each = function(data, callback){
-    var i, l;
-    if (data.constructor === Array){
-      for (i = 0, l = data.length; i < l; i++){
+    var i;
+    if (isArray(data)){
+      for (i = 0; i < data.length; i++){
         callback.call(data, data[i], i, data);
       }
     }else {
@@ -73,80 +59,163 @@
     }
   };
 
+
+  var escapeMap = {
+    '<': '&#60;',
+    '>': '&#62;',
+    '"': '&#34;',
+    "'": '&#39;',
+    '&': '&#38;'
+  };
+
+  /*!
+   * @name    each
+   * @param   {String}  模板字符串
+   * @return  {String}  被转码的模板字符串
+   */
+  var escapeHTML = template.escapeHTML = function(content){
+    return String(content).replace(/&(?![\w#]+;)|[<'">]/g, function($1){
+      return escapeMap[$1];
+    })
+  }
+
+
+  /*!
+   * @name    filter
+   * @param   {String}    管道函数名称
+   * @param   {Function}  管道函数
+   */
   var filter = template.filter = function(name, fn){
     filter[name] = fn;
   };
 
-  var render = template.render = function(tpl, data){
-    var re = new RegExp(tplSetting.openIdentify + '([^' + tplSetting.closeIdentify + ']+)?' + tplSetting.closeIdentify, 'g');
-    var reExpEach = /(^\s?(for|each))(.*)?/g;
-    var reExpIf = /(^\s?(if|else))(.*)?/g;
-    var reExpEnd = /(^\s?(\/if|\/else))(.*)?/g;
-    var reExpEachEnd = /(^\s?(\/for|\/each))(.*)?/g;
-    var reAtt = /[a-zA-Z0-9$_]([a-zA-Z0-9$_]+)?/g;
-    var reExpNotNum = /^[a-zA-Z$_]([a-zA-Z0-9$_]+)?/;
-    var match;
-    var code = 'var r = [];\n';
-    var cursor = 0;
-    each(data, function(v, k){
-      if (reExpNotNum.test(k)) code += 'var ' + k + ' = ' + jsonStringify(v) + ';\n';
+
+  var _cache = template.cache = {};
+
+
+  /*!
+   * @name    render
+   * @param   {String}     模板字符串
+   * @return  {Function}   处理后的渲染模板函数
+   */
+  var render = template.render = function(str){
+    var sTag = template.startTag;
+    var eTag = template.endTag;
+    var $RECHAT = new RegExp(sTag + '([^' + eTag + ']+)?' + eTag, 'g');
+    var preCode = { $each: 'template.each,' };
+    var tempCode = '$out=[];';
+    str = str.replace(/<!--.*-->/g, '');
+    each(str.split(sTag), function(code){
+      code = code.split(eTag);
+      var $0 = code[0];
+      var $1 = code[1];
+      if (code.length === 1){
+        tempCode += html($0);
+      }else {
+        tempCode += getValue($0);
+        tempCode += $1 ? html($1) : '';
+      }
     });
-    var add = function(line, attr){
-      if (attr && line.match(reExpEach)){
-        var arr = line.match(reAtt);
-        var defaultAs = arr[2] == 'as' || arr[2] == 'of';
-        code += 'each(' + arr[1] + ',' + 'function($value, $index){\n';
-        code += 'var $key = $index;\n';
-        if (defaultAs && arr[3] && reExpNotNum.test(arr[3])) code += 'var ' + arr[3] + ' = $value;\n';
-        else throw new Error('The each function as $value cannot begin with Numbers');
-        if (defaultAs && arr[4] && reExpNotNum.test(arr[4])) code += 'var ' + arr[4] + ' = $index;\n';
-        else throw new Error('The each function as $index cannot begin with Numbers');
-        return;
-      }
-      if (attr && line.match(reExpIf)){
-        var arr = line.match(/(\s?else\s+if|\s?if|\s?else|([\s\S]+))/g);
-        code += /^\s?else/.test(arr[0]) ? '}\n' : '';
-        code += arr[1] ? (arr[0] + '(' + arr[1] + ')') : arr[0];
-        code += '{\n';
-        return;
-      }
-      if (attr && line.match(reExpEachEnd)){
-        code += '});\n';
-        return;
-      }
-      if (attr && line.match(reExpEnd)){
-        code += '}\n';
-        return;
-      }
-      var Ltpl = attr ? line : ('"' + line.replace(/"/g, '\\"') + '"');
-      if (Ltpl.indexOf('|') > 0){
-        var tplPipe = Ltpl.split('|');
-        var pipeArr = tplPipe[1].match(/([\s?\$_a-zA-Z0-9]+\:|(.+)?)/g);
-        var pipeFnName = trim(pipeArr[0].replace(':', ''));
-        if (typeof filter[pipeFnName] !== 'function') throw new Error('The ' + pipeFnName + ' is not a function');
-        Ltpl = filter[pipeFnName] + '(' + tplPipe[0] + ',' + pipeArr[1] + ')';
-      }
-      code += 'r.push(' + Ltpl + ');\n';
-    };
-    while(match = re.exec(tpl)){
-      var appendStr = tpl.slice(cursor, match.index);
-      if (appendStr.length) add(appendStr);
-      var attr = trim(match[1]);
-      add(attr, true);
-      cursor = match.index + match[0].length;
+    tempCode += 'return $out.join("")';
+    var preCodes = 'var ';
+    each(preCode, function(v, k){
+      preCodes += k + '=' + v;
+    });
+    var $code = preCodes + tempCode;
+    
+
+    function html(code){
+      return push("'" + code.replace(/('|\\)/g, '\\$1') + "'");
     }
-    add(tpl.substr(cursor, tpl.length - cursor));
-    code += 'return r.join("");';
-    return new Function('each', code.replace(/[\r\n\t]/g, ''))(each);
+
+
+    function push(code){
+      return '$out.push(' + code.replace(/\n/g, '').replace(/\r/g, '') + ');';
+    }
+
+
+    function setValue(code, unshift, pipe){
+      code = code.match(/['"]?[$_a-zA-Z][$_a-zA-Z0-9]*['"]?/g);
+      if (unshift){
+        each(code || [], function(v){
+          if (/^[$_a-zA-Z]/.test(v)) preCode[v] = 'template.' + (pipe ? pipe + '.' : '') + v.replace('$', '') + ','
+        });
+      }else {
+        each(code || [], function(v){
+          if (/^[$_a-zA-Z]/.test(v)) preCode[v] = '$data.' + v + ','
+        });
+      }
+    }
+
+
+    function getValue(code){
+      code = trim(code);
+      var $loc = /^\/?(if|else|each)$|^\/?(if|else|each)[^\S]\s*.*$/;
+      if ($loc.test(code)){
+        var $rc = code.match(/\/?(if|else|each)/)[0];
+        switch ($rc){
+          case 'if':
+            code = code.replace($rc, '');
+            setValue(code);
+            return $rc + '(' + code + '){';
+            break;
+          case 'else':
+            var rc = code.match(/^else\s+if/);
+            code = rc ? code.replace(rc[0], '') : '';
+            setValue(code);
+            return '}' + (rc ? (rc[0] + '(' + code + '){') : $rc + '{');
+            break;
+          case 'each':
+            var $code = trim(code.replace($rc, '')).split(/\s+/), $v = $code[3];
+            if ($v) {
+              $code[3] = $code[2];
+              $code[2] = $v;
+            }
+            var $temp = $code[1] === 'as' ? ($rc + '(' + $code[0] + ',function(' + $code.splice(2) + '){') : ($rc + '(' + $code[0] + ',function($value,$index,$obj){');
+            setValue($code[0]);
+            return '$' + $temp;
+            break;
+          case '/if':
+          case '/else':
+            return '}';
+            break;
+          case '/each':
+            return '});';
+            break;
+          default:
+            return '';
+            break;
+        }
+      }else if (/^.*\|.*/.test(code)){
+        code = code.split('|');
+        var re = /\s*[$_a-zA-Z0-9]+\s*:?/;
+        var func = code[1] ? '$' + trim(code[1].match(re)[0].replace(':', '')) : '';
+        var args = code[1] ? code[1].replace(re, '') : '';
+        args = args ? args.split(',') : [];
+        setValue(func, true, 'filter');
+        each(args, function(v){
+          setValue(v);
+        });
+        return push(func + '(' + code[0] + (args.length ? ',' + args.join(',') : '') + ')');
+      }else {
+        setValue(code);
+        return push(code);
+      }
+    }
+
+
+    var Render = new Function("$data", $code);
+    return Render;
   };
 
-  if (typeof define === 'function') {
-    define(function() {
+
+  // AMD && CMD
+  if (typeof define === 'function'){
+    define(function(){
       return template;
-    });
-  }else if (typeof exports !== 'undefined') {
+    })
+  }else if (typeof module !== 'undefined'){
     module.exports = template;
-  }else {
-    win.template = template;
   }
-})(this);
+  global.template = template;
+})(window || global);
